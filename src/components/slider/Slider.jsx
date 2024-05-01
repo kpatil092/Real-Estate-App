7770fd8af4f973e79e2a491079b64231539a7784
kpatil092 <patilketan2278@gmail.com>
2024-05-01 09:33:10 +0530

    formatting changes

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,62 +1,59 @@
-import React, { useState } from 'react'
-import "./slider.scss"
+import React, { useState } from "react";
+import "./slider.scss";
 
 function Slider({ images }) {
+  const [imgIndex, setImgIndex] = useState(null);
 
-    const [imgIndex, setImgIndex] = useState(null);
-
-    const changeSlide = (direction) => {
-        if(direction === "left") {
-            setImgIndex((images.length + imgIndex - 1) % images.length)
-        } else {
-            setImgIndex((imgIndex + 1) % images.length)
-        }
+  const changeSlide = (direction) => {
+    if (direction === "left") {
+      setImgIndex((images.length + imgIndex - 1) % images.length);
+    } else {
+      setImgIndex((imgIndex + 1) % images.length);
     }
+  };
 
-    return (
-        <div className='slider'>
-            {(imgIndex !== null) &&
-                (<div className="fullSlider">
-                    <div className="arrow">
-                        <img
-                            src="/arrow.png"
-                            alt="arrow"
-                            onClick={() => changeSlide("left")}
-                        />
-                    </div>
-                    <div className="imgContainer">
-                        <img src={images[imgIndex]} alt="img" />
-                    </div>
-                    <div className="arrow">
-                        <img
-                            src="/arrow.png"
-                            alt="arrow"
-                            className='right'
-                            onClick={() => changeSlide("right")}
-                        />
-                    </div>
-                    <div className="close" onClick={() => setImgIndex(null)}>X</div>
-                </div>)
-            }
-            <div className="bigImage">
-                <img
-                    src={images[0]}
-                    alt="img"
-                    onClick={() => setImgIndex(0)}
-                />
-            </div>
-            <div className="smallImages">
-                {images.slice(1).map((image, index) => (
-                    <img
-                        src={image}
-                        alt="img"
-                        key={index}
-                        onClick={() => setImgIndex(index + 1)}
-                    />
-                ))}
-            </div>
+  return (
+    <div className="slider">
+      {imgIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow">
+            <img
+              src="/arrow.png"
+              alt="arrow"
+              onClick={() => changeSlide("left")}
+            />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imgIndex]} alt="img" />
+          </div>
+          <div className="arrow">
+            <img
+              src="/arrow.png"
+              alt="arrow"
+              className="right"
+              onClick={() => changeSlide("right")}
+            />
+          </div>
+          <div className="close" onClick={() => setImgIndex(null)}>
+            X
+          </div>
         </div>
-    )
+      )}
+      <div className="bigImage">
+        <img src={images[0]} alt="img" onClick={() => setImgIndex(0)} />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+          <img
+            src={image}
+            alt="img"
+            key={index}
+            onClick={() => setImgIndex(index + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Slider
+export default Slider;
